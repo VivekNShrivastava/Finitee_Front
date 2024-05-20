83681fc31b87f21bb43c46af1e7ecc112fc18d0a
MEHTAJANVI <janvirajeshmehta@gmail.com>
2024-05-20 12:21:52 +0530

    20-5-24 1st

--- src/app/pages/sidemenu/shopping-listing/shopping-list/shopping-list.page.ts
+++ src/app/pages/sidemenu/shopping-listing/shopping-list/shopping-list.page.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SalesListingService } from 'src/app/core/services/sales-listing/sales-listing.service';
 import { CommonService } from 'src/app/core/services/common.service';
+import { EventItem } from 'src/app/core/models/event/event';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.page.html',
   styleUrls: ['./shopping-list.page.scss'],
 })
 export class ShoppingListPage implements OnInit {
+  navEx: any = {};
+
 
   constructor(private router: Router, 
     public salesService: SalesListingService,
@@ -35,4 +38,17 @@ export class ShoppingListPage implements OnInit {
      
     this.router.navigateByUrl('shopping-list/create-edit-shopping-list')
   }
+  
+
+  openCreateShoppingPage() {
+
+    this.router.navigateByUrl('/shopping-list/create-edit-shopping-list')
+
+    // this.navEx!.state!['data'] = new EventItem();
+    // this.router.navigateByUrl('shopping-listing/create-edit-shopping', this.navEx);
+  }
+
+
+
+  
 }
